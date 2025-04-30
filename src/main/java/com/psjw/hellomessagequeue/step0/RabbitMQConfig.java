6f26bc00fe5fffa6846a79662c15421c0534b27e
psjw <dlrudtn108@gmail.com>
2025-04-30 10:14:58 +0900

    Config Register RabbitTemplate

--- src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
+++ src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.psjw.hellomessagequeue.step0;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +15,11 @@ public class RabbitMQConfig {
     public Queue queue() {
         //durable : 휘발성이냐 아니냐 volatile(휘발성 : false)  or persistent(영속성 : true) 라고 함
         return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        //ConnectionFactory가 RabbitMQ의 연결을 관리하는 객체
+        return new RabbitTemplate(connectionFactory);
     }
 }
