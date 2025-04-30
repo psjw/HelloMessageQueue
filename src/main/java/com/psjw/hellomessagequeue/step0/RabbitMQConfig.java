1f50f3633e80ad1b35b86ca4f068aedfdef9ca0d
psjw <dlrudtn108@gmail.com>
2025-04-30 10:35:53 +0900

    Consumer

--- src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
+++ src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.psjw.hellomessagequeue.step0;
 
-import javax.sound.midi.Receiver;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
