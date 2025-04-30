b0be6838608a99ddf09435d7b594bb6469840585
psjw <dlrudtn108@gmail.com>
2025-04-30 11:33:40 +0900

    Create Step2 Config

--- src/main/java/com/psjw/hellomessagequeue/step2/step1/RabbitMQConfig.java
+++ src/main/java/com/psjw/hellomessagequeue/step2/step1/RabbitMQConfig.java
@@ -1,5 +1,6 @@
-package com.psjw.hellomessagequeue.step1;
+package com.psjw.hellomessagequeue.step2.step1;
 
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -8,15 +9,16 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class RabbitMQConfig {
     // 큐 네임을 설정한다.
-    public static final String QUEUE_NAME = "helloqueue";
+    public static final String QUEUE_NAME = "WorkQueue";
 
     @Bean
     public Queue queue() {
         //durable : 휘발성이냐 아니냐 volatile(휘발성 : false)  or persistent(영속성 : true) 라고 함
-        return new Queue(QUEUE_NAME, false);
+        //서버가 shutdown 됬을때 데이터 보관하는 옵션
+        return new Queue(QUEUE_NAME, true);
     }
 
     @Bean
@@ -33,11 +35,12 @@ public class RabbitMQConfig {
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(QUEUE_NAME);
         container.setMessageListener(listenerAdapter);
+        container.setAcknowledgeMode(AcknowledgeMode.AUTO);
         return container;
     }
 
     @Bean
-    public MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
+    public MessageListenerAdapter listenerAdapter(WorkQueueConsumer workQueueTask) {
+        return new MessageListenerAdapter(workQueueTask, "workQueueTask");
     }
 }
