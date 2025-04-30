d4bcc03557bb1ea249ec5985d5f27e259553c1c3
psjw <dlrudtn108@gmail.com>
2025-04-30 10:17:51 +0900

    Config Register SimpleMessageListenerContainer

--- src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
+++ src/main/java/com/psjw/hellomessagequeue/step0/RabbitMQConfig.java
@@ -3,6 +3,8 @@ package com.psjw.hellomessagequeue.step0;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +23,16 @@ public class RabbitMQConfig {
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         //ConnectionFactory가 RabbitMQ의 연결을 관리하는 객체
         return new RabbitTemplate(connectionFactory);
+    }
+
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(QUEUE_NAME);
+        container.setMessageListener(listenerAdapter);
+        return container;
     }
 }
